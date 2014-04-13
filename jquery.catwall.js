f0e77d56620733cc3f87365be04616b3c8bac411
Tony Ngo <tony2ngo@gmail.com>
2014-04-13 16:46:00 -0700

    Initial commit

---
+++ jquery.catwall.js
@@ -0,0 +1,85 @@
+;(function ( $, window, document, undefined ) {
+	'use strict';
+	
+	var $window = $(window),
+		$document = $(document),
+	
+	    // Create the defaults once
+		pluginName = 'catWall',
+        defaults = {
+			infiniteScroll: true,
+			pageSize: 30,
+			imageSrc: undefined
+        };
+
+	// Binds an instance of CatWall to load page
+	// when the user scrolls to the bottom of the page
+	function bind(instance) {
+		$window.scroll(function (event) {
+			if($window.scrollTop() + $window.height() == $document.height()) {
+				instance.loadPage();
+			}
+		});
+	}
+
+    // The actual plugin constructor
+    function Plugin( element, options ) {
+		this.$element = $(element);
+        this.element = element;
+        this.options = $.extend( {}, defaults, options) ;
+        this._defaults = defaults;
+        this._name = pluginName;
+        
+        this.init();
+    }
+
+	// Initialization
+    Plugin.prototype.init = function () {
+		this.$element.addClass('catwall');
+		this.loadPage();
+		
+		if (this.options.infiniteScroll) {
+			bind(this);
+		}
+    };
+	
+	// Creates an array of images of the given size
+	// Uses time since epoch as cache buster
+	Plugin.prototype.createImages = function (arraySize) {
+		var images = [];
+		var t = (new Date()).getTime();
+
+		for (var i = arraySize; i--;) {
+			var image = new Image();
+			image.src = this.options.imageSrc + '&v=' + t + i;
+			images.push(image);
+		}
+		
+		return images;
+	};
+	
+	// Append images to the wall
+	Plugin.prototype.loadPage = function () {
+		var images = this.createImages(this.options.pageSize);
+		
+		// Wrap images
+		images = images.map(function (image) {
+			return $('<div>', { 'class': 'catwall-wrapper' }).append(image);
+		});
+		
+		// jQuery uses document fragment to add elements in bulk
+		this.$element.append(images);
+	};
+
+    // A really lightweight plugin wrapper around the constructor, 
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, 
+                new Plugin( this, options ));
+            }
+        });
+    }
+
+})( jQuery, window, document );
